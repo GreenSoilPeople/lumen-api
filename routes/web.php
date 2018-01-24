009de3c7e78fadf4bdda05b78f94cef0a8bdfb0e
Bogdan Triculescu <bogdan.triculescu@gmail.com>
2018-01-25 01:47:18 +0200

    lumen api test

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->group(['prefix' => 'api/v1'], function($router) {
+    $router->post('posts/add', 'PostsController@createPost');
+    $router->get('posts/view/{id}', 'PostsController@viewPost');
+    $router->put('posts/edit/{id}', 'PostsController@updatePost');
+    $router->delete('posts/delete/{id}', 'PostsController@deletePost');
+    $router->get('posts/index', 'PostsController@index');
+});
